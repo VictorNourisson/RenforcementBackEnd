5248f4f72eaf566f8a9e72002fb8f41d65dee963
badaboom21 <130473771+badaboom21@users.noreply.github.com>
2026-03-24 16:09:26 +0100

    second commit

---
+++ assurmoi/migrations/20260324102247-user-table.js
@@ -0,0 +1,50 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    queryInterface.createTable("users", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      username: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      firstname: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      lastname: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      email: {
+        type: Sequelize.STRING,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.dropTable("users");
+  },
+};
---
+++ assurmoi/models/users.js
@@ -0,0 +1,45 @@
+const { Model, DataTypes } = require("sequelize");
+
+const User = (dbInstance, DataTypes) => {
+  class User extends Model {
+    // Jointure avec Person
+    // static associate(models) {
+    //     this.belongsTo(models.Person, {
+    //         foreignKey: 'person_id',
+    //         as: 'Person',
+    //     })
+    // }
+  }
+
+  User.init(
+    {
+      username: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      firstname: {
+        type: DataTypes.STRING(50),
+        allowNull: true,
+      },
+      lastname: {
+        type: DataTypes.STRING(50),
+        allowNull: true,
+      },
+      email: {
+        type: DataTypes.STRING(50),
+        allowNull: true,
+      },
+    },
+    {
+      sequelize: dbInstance,
+      modelName: "User",
+    },
+  );
+  return User;
+};
+
+module.exports = User;
